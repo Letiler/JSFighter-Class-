a7eea85a43a3674c787b07649eb74fd0426d5982
Micah <micah2021190@live.ytech.edu>
2019-11-12 11:19:34 -0500

    added imgs

--- script/index.js
+++ script/index.js
@@ -49,6 +49,9 @@ function startup() {
   Player0 = new Fighter(P0NAME, P0CHARA);
   Player1 = new Fighter(P1NAME, P1CHARA);
 
+document.getElementById('graphicsBox').innerHTML = '<img src="img/' + P0CHARA + '_idle.png"><img src="img/saml_idle.png" alt="sam" class="fighterIMG">'
+
+
   console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
   console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
 }
